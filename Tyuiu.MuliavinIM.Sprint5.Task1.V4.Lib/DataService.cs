440d9e94ed8f8cb3c57a03d49414c992865462e9
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-12-12 19:43:25 +0500

    Task1

---
+++ Tyuiu.MuliavinIM.Sprint5.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MuliavinIM.Sprint5.Task1.V4.Lib;
+public class DataService : ISprint5Task1V4
+{
+    public string SaveToFileTextData(int startValue, int stopValue)
+    {
+        string output = string.Empty;
+        for (int i = startValue; i <= stopValue; i++)
+        {
+            double value;
+            if (i != -1)
+                value = Math.Round(f(i), 2);
+            else
+                value = 0;
+            output += value + "\n";
+        }
+
+        output = output[..^1];
+
+        string path = Path.GetTempFileName();
+
+        File.WriteAllText(path, output);
+
+        return output;
+    }
+
+    private double f(double x) => Math.Cos(x) / (x + 1) - Math.Cos(x) * 1.3 + 3 * x;
+}
---
+++ Tyuiu.MuliavinIM.Sprint5.Task1.V4/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MuliavinIM.Sprint5.Task1.V4.Lib;
+
+Console.Title = "Спринт #5 | Выполнил: Мулявин.И.М.  | ИСТНб-25-1 ";
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* Спринт #5                                                            *");
+Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл              *");
+Console.WriteLine("* Задание #1                                                           *");
+Console.WriteLine("* Вариант #4                                                           *");
+Console.WriteLine("* Выполнил: Мулявин Иван Михайлович | ИСТНб-25-1                       *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                             *");
+Console.WriteLine("* Дана функция, произвести табулирование.                              *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+Console.WriteLine("************************************************************************");
+
+Console.WriteLine(new DataService().SaveToFileTextData(-5, 5));
+Console.ReadKey();
