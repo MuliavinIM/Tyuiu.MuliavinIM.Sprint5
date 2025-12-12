e2a114b881531a02991dbaa82deb4b7358e8017d
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-12-12 20:20:20 +0500

    Task7

---
+++ Tyuiu.MuliavinIM.Sprint5.Task7.V28/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MuliavinIM.Sprint5.Task7.V28.Lib;
+namespace Tyuiu.MuliavinIM.Sprint5.Task7.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Мулявин.И.М.  | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Мулявин Иван Михайлович |  ИСТНб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Заменить все пробелы, идущие подряд более одного, на один пробел.       *");
+            Console.WriteLine("* Полученный результат сохранить в файл OutPutDataFileTask7V28.txt.       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            string path = Path.Combine(new string[] { "C:", "DataSprint5", "InPutDataFileTask7V28.txt" });
+            string pathSaveFile = Path.Combine(new string[] { "C:", "DataSprint5", "InPutDataFileTask7V28.txt" });
+
+            Console.WriteLine("Данные находтся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            pathSaveFile = ds.LoadDataAndSave(pathSaveFile);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
