36f465ec5b6099ba3edfbffe319e85d672779c07
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-12-12 19:59:29 +0500

    Task5

---
+++ Tyuiu.MuliavinIM.Sprint5.Task5.V29.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MuliavinIM.Sprint5.Task5.V29.Lib
+{
+    public class DataService : ISprint5Task5V29
+    {
+        public double LoadFromDataFile(string path)
+        {
+            int min = 1000;
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string lineReplace = line.Replace('.', ',');
+                    string[] lineArray = lineReplace.Split(' ');
+
+                    foreach (string number in lineArray)
+                    {
+                        double lineParse = double.Parse(number);
+
+                        if ((lineParse > 9) && (lineParse < 100))
+                        {
+                            res = lineParse;
+                            while (res < min)
+                            {
+                                min = (int)res;
+                            }
+                        }
+                    }
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Tyuiu.MuliavinIM.Sprint5.Task5.V29.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Tyuiu.MuliavinIM.Sprint5.Task5.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
